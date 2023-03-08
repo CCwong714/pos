38f27bb2085645339eb40b5c011de5459424bc38
unknown <snow714@outlook.com>
2023-03-08 21:52:55 +0800

    push backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
 import React from "react";
 import logo from "./logo.svg";
-import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Image, Text } from "@chakra-ui/react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "./index";
 import uuid from "react-uuid";
 import { setTable1 } from "./features/tableSlice";
 import OrderComponent from "./components/tableOrder/OrderComponent";
+import ChickenRice from "./assets/chickenRice.jpg";
+import Json from "./json/order.json";
 
 function App() {
   const dispatch = useDispatch();
   const table1 = useSelector((state: RootState) => state.table.table1.order);
   console.log(table1);
+  console.log(Json);
   return (
     <Box>
-      <OrderComponent Name="watermalon" />
-      <OrderComponent Name="apple" />
-      <OrderComponent Name="orange" />
+      <OrderComponent
+        Name="Chicken Rice"
+        src={ChickenRice}
+        itemPrice={1 + 1 + 2 + 3}
+      >
+        <>
+          {Json.data[0].PCSO.map((item: any, index: number) => {
+            return <Checkbox value={item.id}>{item.id}</Checkbox>;
+          })}
+        </>
+        <Checkbox value="Add Rice">Add Rice</Checkbox>
+        <Checkbox value="Add Egg">Add Egg</Checkbox>
+        <Checkbox value="Add Chicken">Add Chicken</Checkbox>
+        <Checkbox value="Add Pig">Add Pig</Checkbox>
+        <Checkbox value="Add HotDog">Add Hotdog</Checkbox>
+      </OrderComponent>
+      <OrderComponent Name="apple" src={ChickenRice} itemPrice={1 + 2 + 3 + 4}>
+        <Text>123</Text>
+      </OrderComponent>
+      <OrderComponent Name="orange" src={ChickenRice} itemPrice={4 + 1 + 3 + 3}>
+        <Text>123</Text>
+      </OrderComponent>
       <Box>
-        {table1.map((item: any) => {
+        {table1.map((item: any, index: number) => {
           return (
-            <Box>
+            <Box key={index}>
               <>{console.log("123", item)}</>
               <Flex flexDirection="row" justifyContent="space-between">
                 <Text color="green.900">{item.name}</Text>
--- src/components/tableOrder/OrderComponent.tsx
+++ src/components/tableOrder/OrderComponent.tsx
@@ -1,5 +1,24 @@
-import { Box, Button, Text } from "@chakra-ui/react";
-import React, { FC } from "react";
+import {
+  Box,
+  Button,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React, {
+  Children,
+  FC,
+  ReactChild,
+  ReactChildren,
+  ReactNode,
+} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import uuid from "react-uuid";
 import { RootState } from "../..";
@@ -7,14 +26,20 @@ import { setTable1 } from "../../features/tableSlice";
 
 interface OrderType {
   Name: string;
+  children: ReactNode;
+  src: string;
+  itemPrice: string | number;
 }
 
-const OrderComponent: FC<OrderType> = ({ Name }) => {
+const OrderComponent: FC<OrderType> = ({ Name, children, src, itemPrice }) => {
   const dispatch = useDispatch();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Box>
-      <Text>{Name}</Text>
-      <Button
+    <>
+      <Box onClick={onOpen}>
+        <Text fontSize={24}>{Name}</Text>
+        <Image src={src} height={150} width={150} />
+        {/* <Button
         backgroundColor="#00FF91"
         onClick={() => {
           dispatch(
@@ -23,8 +48,46 @@ const OrderComponent: FC<OrderType> = ({ Name }) => {
         }}
       >
         +
-      </Button>
-    </Box>
+      </Button> */}
+      </Box>
+
+      <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+
+        <ModalContent padding={5}>
+          <ModalBody>
+            <>
+              <Image src={src} height={150} width="100%" />
+              <Text
+                fontSize={21}
+                fontWeight="bold"
+                textAlign="right"
+                // color="blue.500"
+              >
+                RM {itemPrice}
+              </Text>
+              <Text fontSize={18} fontWeight="bold">
+                Chicken Rice
+              </Text>
+              {children}
+            </>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              backgroundColor="blue.500"
+              color="white"
+              marginRight={5}
+              onClick={() => {
+                console.log("add done");
+              }}
+            >
+              Add
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
